10833fb2662e67b186b6bab12fa258c9a5054e89
DominikBBB <dominik.brzeski@wp.eu>
2022-07-25 01:19:23 +0000

    Refactor update worksheet functions into one

--- run.py
+++ run.py
@@ -70,15 +70,36 @@ def validate_data(values):
 
     return True
 
-
+"""
 def update_sales_worksheet(data):
-    """
+    '''
     Update sales worksheet, add new row with the list data provided
-    """
+    '''
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
+
+
+def update_surplus_worksheet(data):
+    '''
+    Update surplus worksheet, add new row with the list data provided
+    '''
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
+"""
+
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -96,24 +117,12 @@ def calculate_surplus_data(sales_row):
     #print(stock_row)
     #print(f'stock row: {stock_row}')
     #print(f'sales row: {sales_row}')
-
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
     #print(surplus_data)
     return surplus_data
-
-
-def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with the list data provided
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully.\n")
-
 
 
 def main ():
@@ -123,10 +132,12 @@ def main ():
     data = get_sales_data()
     #print(data)
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    #update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     #print(new_surplus_data)
-    update_surplus_worksheet(new_surplus_data)
+    #update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 
 print("Welcome to Love Sandwiches Data Automation! \n")
