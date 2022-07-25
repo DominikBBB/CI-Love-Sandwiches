87a1a420cfae8687af6129af1610805aa1e820d0
DominikBBB <dominik.brzeski@wp.eu>
2022-07-25 02:45:49 +0000

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -125,19 +125,6 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
-def main ():
-    """
-    Run all program functions
-    """
-    data = get_sales_data()
-    #print(data)
-    sales_data = [int(num) for num in data]
-    #update_sales_worksheet(sales_data)
-    update_worksheet(sales_data, "sales")
-    new_surplus_data = calculate_surplus_data(sales_data)
-    #print(new_surplus_data)
-    #update_surplus_worksheet(new_surplus_data)
-    update_worksheet(new_surplus_data, "surplus")
 
 
 def get_last_5_entries_sales():
@@ -157,8 +144,46 @@ def get_last_5_entries_sales():
     # pprint(columns)
     return columns
 
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    # print(new_stock_data)
+    return new_stock_data
+    
+
+
+def main ():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    #print(data)
+    sales_data = [int(num) for num in data]
+    #update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
+    new_surplus_data = calculate_surplus_data(sales_data)
+    #print(new_surplus_data)
+    #update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    # print(stock_data)
+    update_worksheet(stock_data, "stock")
+
 print("Welcome to Love Sandwiches Data Automation! \n")
-#main()
+main()
 
 # get_last_5_entries_sales()
-sales_columns = get_last_5_entries_sales()
+# sales_columns = get_last_5_entries_sales()
+# stock_data = calculate_stock_data(sales_columns)
